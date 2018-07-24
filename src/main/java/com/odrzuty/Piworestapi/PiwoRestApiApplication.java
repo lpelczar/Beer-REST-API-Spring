30d2cce259ae7537a0d783d0898e8e7e103c9e75
filipHartman <filip.hartman23@gmail.com>
2018-07-24 11:16:14 +0200

    Create project for piwo rest api

---
+++ src/main/java/com/odrzuty/Piworestapi/PiwoRestApiApplication.java
@@ -0,0 +1,12 @@
+package com.odrzuty.Piworestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiwoRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiwoRestApiApplication.class, args);
+	}
+}
