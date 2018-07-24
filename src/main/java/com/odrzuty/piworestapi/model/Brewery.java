0e5a8f29e4d77555077f206126e5cea7a6ca786e
lpelczar <lukepelczar@gmail.com>
2018-07-24 13:13:58 +0200

    Add brewery model

---
+++ src/main/java/com/odrzuty/piworestapi/model/Brewery.java
@@ -0,0 +1,81 @@
+package com.odrzuty.piworestapi.model;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Entity
+@Table(name = "breweries")
+@Setter
+@Getter
+@ToString
+public class Brewery {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Size(max = 255)
+    String name;
+
+    @Size(max = 255)
+    String address1;
+
+    @Size(max = 255)
+    String city;
+
+    @Size(max = 255)
+    String state;
+
+    @Size(max = 25)
+    String code;
+
+    @Size(max = 255)
+    String country;
+
+    @Size(max = 50)
+    String phone;
+
+    @Size(max = 255)
+    String website;
+
+    String descript;
+
+    @OneToMany(mappedBy = "brewery", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JsonIgnore
+    List<Beer> beers;
+
+    public Brewery() {
+
+    }
+
+    public Brewery(int id) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("beersJPA");
+        EntityManager entityManager = emf.createEntityManager();
+        entityManager.getTransaction().begin();
+
+        Brewery brewery = entityManager.find(Brewery.class, id);
+
+        this.id = brewery.id;
+        this.name = brewery.name;
+        this.address1 = brewery.address1;
+        this.city = brewery.city;
+        this.state = brewery.state;
+        this.code = brewery.code;
+        this.country = brewery.country;
+        this.phone = brewery.phone;
+        this.website = brewery.website;
+        this.descript = brewery.descript;
+
+
+        entityManager.getTransaction().commit();
+
+    }
+
+}
