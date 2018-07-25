0cfcde41ead6b0ab5dc997c1d6e82ce6744a469d
staporekmarta <staporekmarta1@gmail.com>
2018-07-25 13:57:11 +0200

    Add method in controller to save removed beer

--- src/main/java/com/odrzuty/piworestapi/controller/BeerRestController.java
+++ src/main/java/com/odrzuty/piworestapi/controller/BeerRestController.java
@@ -2,7 +2,9 @@ package com.odrzuty.piworestapi.controller;
 
 import com.odrzuty.piworestapi.exception.ResourceNotFoundException;
 import com.odrzuty.piworestapi.model.Beer;
+import com.odrzuty.piworestapi.model.removed.RemovedBeer;
 import com.odrzuty.piworestapi.repository.BeerRepository;
+import com.odrzuty.piworestapi.repository.removed.RemovedBeerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,10 +18,12 @@ import java.util.Collection;
 public class BeerRestController {
 
     private final BeerRepository beerRepository;
+    private final RemovedBeerRepository removedBeerRepository;
 
     @Autowired
-    public BeerRestController(BeerRepository beerRepository) {
+    public BeerRestController(BeerRepository beerRepository, RemovedBeerRepository removedBeerRepository) {
         this.beerRepository = beerRepository;
+        this.removedBeerRepository = removedBeerRepository;
     }
 
     @GetMapping(value = "/beers", produces = "application/json")
@@ -55,8 +59,18 @@ public class BeerRestController {
         Beer beer = beerRepository.findById(beerId)
                 .orElseThrow(() -> new ResourceNotFoundException("Beer", "id", beerId));
 
+        saveRemoved(beer);
+
         beerRepository.delete(beer);
 
         return ResponseEntity.ok().build();
+    }
+
+    private void saveRemoved(Beer beer) {
+
+        String breweryName = beer.getBrewery().getName();
+        String categoryName = beer.getCategory().getName();
+        String styleName = beer.getStyle().getName();
+        removedBeerRepository.save(new RemovedBeer(breweryName, categoryName, styleName));
     }
 }
