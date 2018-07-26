054d22abbc8e2ec27b62ed59de6600a081d8469c
filipHartman <filip.hartman23@gmail.com>
2018-07-26 21:25:12 +0200

    Add implementation for logger service using log4j2 library

---
+++ src/main/java/com/odrzuty/piworestapi/service/LoggerServiceLog4J.java
@@ -0,0 +1,22 @@
+package com.odrzuty.piworestapi.service;
+
+import com.odrzuty.piworestapi.Application;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.Marker;
+import org.apache.logging.log4j.MarkerManager;
+
+public class LoggerServiceLog4J implements LoggerService {
+
+    private static final Logger logger = LogManager.getLogger(Application.class);
+    private Marker marker  = MarkerManager.getMarker("RUNTIME");
+    @Override
+    public void logError(String message) {
+        logger.error(marker, message);
+    }
+
+    @Override
+    public void logInfo(String message) {
+        logger.info(marker, message);
+    }
+}
