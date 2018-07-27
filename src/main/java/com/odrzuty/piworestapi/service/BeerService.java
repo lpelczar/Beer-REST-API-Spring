ca02a99891ecfb7109b50505183307e6997df65b
filipHartman <filip.hartman23@gmail.com>
2018-07-27 10:17:57 +0200

    Add methods signatures for services

--- src/main/java/com/odrzuty/piworestapi/service/BeerService.java
+++ src/main/java/com/odrzuty/piworestapi/service/BeerService.java
@@ -1,4 +1,12 @@
 package com.odrzuty.piworestapi.service;
 
+import com.odrzuty.piworestapi.model.Beer;
+
+import java.util.List;
+
 public interface BeerService {
+    List<Beer> findAll();
+    Beer save(Beer beer);
+    Beer findById(int beerId);
+    void delete(Beer beer);
 }
--- src/main/java/com/odrzuty/piworestapi/service/CategoryService.java
+++ src/main/java/com/odrzuty/piworestapi/service/CategoryService.java
@@ -1,4 +1,12 @@
 package com.odrzuty.piworestapi.service;
 
+import com.odrzuty.piworestapi.model.Category;
+
+import java.util.List;
+
 public interface CategoryService {
+    List<Category> findAll();
+    Category save(Category category);
+    Category findById(int categoryId);
+    void delete(Category category);
 }
