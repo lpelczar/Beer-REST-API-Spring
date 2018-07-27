9e415403dc76d029b92b185f63e6e96d2aca83ea
lpelczar <lukepelczar@gmail.com>
2018-07-27 14:14:13 +0200

    Add logging exceptions

--- src/main/java/com/odrzuty/piworestapi/exception/BeerRestExceptionHandler.java
+++ src/main/java/com/odrzuty/piworestapi/exception/BeerRestExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.odrzuty.piworestapi.exception;
 
+import com.odrzuty.piworestapi.service.LoggerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -8,6 +10,13 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class BeerRestExceptionHandler {
 
+    private final LoggerService loggerService;
+
+    @Autowired
+    public BeerRestExceptionHandler(LoggerService loggerService) {
+        this.loggerService = loggerService;
+    }
+
     @ExceptionHandler
     public ResponseEntity<BeerErrorResponse> handleException(ResourceNotFoundException e) {
 
@@ -15,6 +24,8 @@ public class BeerRestExceptionHandler {
         beerErrorResponse.setStatus(HttpStatus.NOT_FOUND.value());
         beerErrorResponse.setMessage(e.getMessage());
         beerErrorResponse.setTimeStamp(System.currentTimeMillis());
+
+        loggerService.logError("(APPLICATION) Resource not found exception: " + beerErrorResponse.getMessage());
 
         return new ResponseEntity<>(beerErrorResponse, HttpStatus.NOT_FOUND);
     }
